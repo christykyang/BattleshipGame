dfe35ec293ad91da9e4bbaec7edefab90eb06679
Matthew VH <vanheckematt@gmail.com>
2020-06-19 11:41:57 -0500

    Created Board model, and added methods for CreateGame() and PlaceShip() to PlayerController.

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Battleship.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Battleship.Controllers
 {
     public class PlayerController : Controller
     {
+        public string[][] boardOne;
+        public string[][] boardTwo;
+        public PlayerController()
+        {
+            
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateGame()
+        {
+            boardOne = new Board(20, 20).board;
+            boardTwo = new Board(20, 20).board;
+            return View();
+        }
+        public IActionResult PlaceShip()
         {
             return View();
         }
---
+++ Models/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battleship.Models
+{
+    public class Board
+    {
+        public string[][] board;
+        public Board(int height, int width, string value = "0")
+        {
+            board = CreateBoard(height, width, value);
+        }
+        public string[][] CreateBoard(int height, int width, string value = "0")
+        {
+            string[][] board = new string[height][];
+            for(int i = 0; i < height; i++)
+            {
+                board[i] = new string[width];
+                for(int j = 0; j < board[i].Length; j++)
+                {
+                    board[i][j] = value;
+                }
+            }
+            return board;
+        }
+    }
+}
