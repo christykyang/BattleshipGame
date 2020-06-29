d057e324ec1b327513821c1958908dec39ba3808
Matthew VH <vanheckematt@gmail.com>
2020-06-29 10:26:49 -0500

    Added GameViewModel that contains Player1Board and Player2Board as Board objects instead of strings. Replaced player1Board and player2Board variables in PlayerController with a GameViewModel object.

---
+++ Models/GameViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battleship.Models
+{
+    public class GameViewModel
+    {
+        public int Id { get; set; }
+        public int Player1Id { get; set; }
+        public int Player2Id { get; set; }
+        public Board Player1Board { get; set; }
+        public Board Player2Board { get; set; }
+    }
+}
